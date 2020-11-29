6fa7499a2dfaea2c24b57c662ed4439deb2c27ed
Glitch (blossom-carbonated-alder) <none>
2020-11-29 19:44:06 +0000

    ⏰🍩 Updated with Glitch

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
+const cors = require('cors')
+app.use(cors())
 
 let morgan = require('morgan')
 app.use(morgan('combined'))
@@ -15,7 +17,12 @@ app.use(bodyParser.raw({ type: "*/*" }))
 let credentials = new Map()
 let tokens = new Map()
 let channels = new Set()
-let usersInChannel = new Set()
+let usersInChannel = new Map()
+let joined = []
+
+app.get("/sourcecode", (req, res) => {
+  res.send(require('fs').readFileSync(__filename).toString())
+})
 
 // This endpoint lets users create an account
 app.post("/signup", (req, res) => {
@@ -76,7 +83,7 @@ app.post("/login", (req, res) => {
     }
   
     token = username + Math.random().toString(36).substr(2)
-    tokens.set(username, token)
+    tokens.set(token, username)
     console.log("TOKENS: ")
     console.log(tokens)
     res.send(JSON.stringify({ success: true, token: token }))
@@ -148,10 +155,14 @@ app.post("/join-channel", (req, res) => {
       return
     }
   
-    usersInChannel.add(tokenId+channelName)
+    usersInChannel.set(tokenId+channelName, tokens.get(tokenId))
     res.send(JSON.stringify({ success: true }))
     console.log("USERS IN CHANNEL: ")
     console.log(usersInChannel)
+  
+    console.log("JOINED: ")
+    joined.push({"channelName":channelName, "user": tokens.get(tokenId)})
+    console.log(joined)
   
 })
 
@@ -193,6 +204,47 @@ app.post("/leave-channel", (req, res) => {
     console.log("USERS IN CHANNEL AFTER REMOVAL: ")
     console.log(usersInChannel)
   
+})
+
+//The joined endpoint provides the usernames of everyone in the channel.
+app.get("/joined", (req, res) => {
+    let channelName = req.query.channelName
+    let tokenId = req.headers.token
+    
+    if (tokenId == undefined) {
+      res.send(JSON.stringify({ success: false, reason: "token field missing" }))
+      return
+    }
+  
+    if (!tokens.has(tokenId)) {
+      res.send(JSON.stringify({ success: false, reason: "Invalid token" }))
+      return
+    }
+  
+    if (channelName == undefined) {
+      res.send(JSON.stringify({ success: false, reason: "channelName field missing" }))
+      return
+    }
+  
+    if (!channels.has(channelName)) {
+        res.send(JSON.stringify({ success: false, reason: "Channel does not exist" }))
+        return
+    }
+  
+    if (!usersInChannel.has(tokenId+channelName)) {
+      res.send(JSON.stringify({ success: false, reason: "User is not part of this channel" }))
+      return
+    }
+  
+    let list = []
+    for (let i = 0; i < joined.length; i++){
+      if(joined[i].channelName == channelName) {
+        list.push(joined[i].user)
+      }
+    }
+    console.log("LIST: " + list)
+    //res.send(JSON.stringify({ success: true, joined: JSON.stringify(list)}))
+    res.send(JSON.stringify({ success: true, joined: list}))
 })
 
 
